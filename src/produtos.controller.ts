5359c9ccb4f79c2e9a49cd2c3bcd7c7831f6ce8d
TI SATC <deptotisatc@gmail.com>
2021-07-06 02:25:19 -0300

    feat: models

--- src/produtos.controller.ts
+++ src/produtos.controller.ts
@@ -1,33 +1,39 @@
 import { Body, Controller, Delete, Get, Param, Post, Put } from "@nestjs/common";
+import { Produto } from './produto.model';
+
 
 @Controller('produtos')
 export class ProdutosController {
+    produtos: Produto[] = [
+        new Produto('LIV01', "Livro TDD e BDD na prática", 29.90),
+        new Produto('LIV02', "Livro TDD e BDD na prática vol II", 39.90),
+        new Produto('LIV03', "Livro TDD e BDD na prática vol III", 34.90)        
+    ];
 
     @Get()
-    obterTodos(): string{
-        return 'Lista todos os produtos';
+    obterTodos(): Produto[]{
+        return this.produtos;
     }
 
     @Get(':id')
-    obterUm(@Param() params): string{
-        return `Lista produto com id ${params.id}`;
+    obterUm(@Param() params): Produto{
+        return this.produtos[0];
     }
 
     @Post()
-    criar(@Body() produto): string {
-        console.log(produto);
-        return 'Produto criado';
+    criar(@Body() produto) {
+        produto.id = 100;        
+        this.produtos.push(produto);
     }
 
     @Put()
-    alterar(@Body() produto): string {
-        console.log(produto);
-        return 'Produto atualizado';
+    alterar(@Body() produto: Produto): Produto {
+        return produto;
     }
 
     @Delete(':id')
-    apagar(@Param() params): string {
-        return `Apaga o produto ${params.id}`;
+    apagar(@Param() params) {
+        this.produtos.pop();
     }
 
 }
